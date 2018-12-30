a3a49ee6b495460cc1d4f6cb4e082360076ce090
Zhengqun Koo zhengqun.koo@gmail.com <Zhengqun Koo zhengqun.koo@gmail.com>
2018-12-31 02:10:27 +0800

    Add interpreter and processor
    Hardcode choice for now
    Eventually let user input their choice

--- ecce/reactive.hs
+++ ecce/reactive.hs
@@ -1,51 +1,88 @@
-{-----------------------------------------------------------------------------
-    reactive-banana
-    
-    Example: Actuate and pause an event network
-------------------------------------------------------------------------------}
-import Control.Monad (when)
-import Data.IORef
-import Data.List (nub)
-import Data.Maybe (fromJust, isJust)
-import Debug.Trace
-import System.IO
-import System.Random
+{-
+ - SECTION PRAGMAS
+ -}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-} -- Allows type signatures in patterns
 
-import Reactive.Banana
+{-
+ - SECTION IMPORTS
+ -}
+import Backend
+  ( Expr(EGlobalProtocolChoice, EGlobalProtocolConcurrency,
+     EGlobalProtocolSequencing)
+  , Expr
+  , GlobalProtocol
+  , extractFile
+  , extractParse
+  , parseCommand
+  , parseGlobalProtocol
+  , parseRestInputLine
+  , welcome
+  )
+import Control.Monad (join, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Either (isLeft, rights)
+
+-- import Debug.Trace (trace)
+import Reactive.Banana (accumE, compile)
 import Reactive.Banana.Frameworks
+  ( AddHandler
+  , EventNetwork
+  , MomentIO
+  , actuate
+  , fromAddHandler
+  , newAddHandler
+  , reactimate
+  )
+import System.IO (FilePath)
+import Text.Parsec (ParseError)
 
+{-
+ - SECTION USER INTERFACE
+ -}
 main :: IO ()
 main = do
-  displayHelpMessage
-  sources <- (,) <$> newAddHandler <*> newAddHandler
-  network <- setupNetwork sources
-  actuate network
-  eventLoop sources network
+  welcome
+  interpreter
 
-displayHelpMessage :: IO ()
-displayHelpMessage =
-  mapM_ putStrLn $
-  "Commands are:" :
-  "   count   - send counter event" :
-  "   pause   - pause event network" :
-  "   actuate - actuate event network" :
-  "   quit    - quit the program" : "" : []
+interpreter :: IO ()
+interpreter = do
+  putStr "ecce> "
+  inputLine <- getLine
+  Main.interpret inputLine
+  interpreter
+
+interpret :: String -> IO ()
+interpret inputLine =
+  case command of
+    Nothing -> mapM_ putStrLn $ "Here are a list of commands:" : commands
+    Just "help" -> mapM_ putStrLn $ "Here are a list of commands:" : commands
+    Just "load" -> do
+      sources <- newAddHandler
+      network <- compile $ networkDescription sources restInputLine
+      actuate network
+      eventLoop sources network
+    -- TODO fix double parsing
+    -- TODO find better way to extract parsed expression than (Right .. =)
+  where
+    Right command = extractParse parseCommand inputLine
+    Right restInputLine = extractParse parseRestInputLine inputLine
 
 -- Read commands and fire corresponding events 
-eventLoop :: (EventSource (), EventSource EventNetwork) -> EventNetwork -> IO ()
-eventLoop (escounter, espause) network = loop
+eventLoop :: EventSource () -> EventNetwork -> IO ()
+eventLoop esstepper network = loop
   where
     loop = do
       putStr "> "
-      hFlush stdout
-      s <- getLine
-      case s of
-        "count" -> fire escounter ()
-        "pause" -> fire espause network
-        "actuate" -> actuate network
-        "quit" -> return ()
-        _ -> putStrLn $ s ++ " - unknown command"
-      when (s /= "quit") loop
+      c <- getChar
+      case c of
+        's' -> fire esstepper ()
+        'q' -> putStrLn "" >> return ()
+        otherwise -> putStrLn " - unknown character"
+      when (c /= 'q') loop
+
+commands :: [String]
+commands = "help" : "load" : []
 
 {-----------------------------------------------------------------------------
     Event sources
@@ -63,12 +100,50 @@ fire = snd
 {-----------------------------------------------------------------------------
     Program logic
 ------------------------------------------------------------------------------}
+-- Each process sequentially resolves transmissions
+-- Each process can also have many concurrently running processes
+data Process
+  = ProcessLeaf (Expr GlobalProtocol)
+  | ProcessNode [Process]
+  deriving (Show)
+
 -- Set up the program logic in terms of events and behaviors.
-setupNetwork :: (EventSource (), EventSource EventNetwork) -> IO EventNetwork
-setupNetwork (escounter, espause) =
-  compile $ do
-    ecounter <- fromAddHandler (addHandler escounter)
-    epause <- fromAddHandler (addHandler espause)
-    ecount <- accumE 0 $ (+ 1) <$ ecounter
-    reactimate $ fmap print ecount
-    reactimate $ fmap pause epause
+networkDescription :: EventSource () -> FilePath -> MomentIO ()
+networkDescription esstepper restInputLine = do
+  xs <- liftIO $ extractFile restInputLine
+  liftIO $
+    either
+      (mapM_ putStrLn)
+      (\xs ->
+         let gs = map (extractParse parseGlobalProtocol) xs
+          in if any isLeft gs
+               then putStrLn "some error"
+               else mapM_ putStrLn $
+                    processGlobalProtocol
+                      (ProcessNode $ map ProcessLeaf (rights gs)))
+      xs
+  estepper <- fromAddHandler (addHandler esstepper)
+  estep <- accumE 0 $ (+ 1) <$ estepper
+  reactimate $ fmap print estep
+
+processGlobalProtocol :: Process -> [String]
+processGlobalProtocol p =
+  let aux :: [String] -> Process -> [String]
+      aux ss (ProcessLeaf g)
+        -- trace ("leaf: " ++ show g) $
+       =
+        case g of
+          EGlobalProtocolConcurrency g1 g2 ->
+            aux
+              ss
+              (ProcessNode
+                 [ProcessNode [ProcessLeaf g1], ProcessNode [ProcessLeaf g2]])
+          EGlobalProtocolChoice g1 g2 -> aux ss (ProcessLeaf g2) -- TODO Unhardcode choice to g2
+          EGlobalProtocolSequencing g1 g2 ->
+            aux ss (ProcessNode [ProcessLeaf g1, ProcessLeaf g2])
+          otherwise -> show g : ss
+      aux ss (ProcessNode []) = ss
+      aux ss (ProcessNode (p:ps))
+        -- trace ("node: " ++ show (p : ps)) $
+       = aux (join $ ss : aux [] p : []) (ProcessNode ps)
+   in aux [] p

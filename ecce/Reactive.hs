a2aa3c870d0511570b2688a3d898e7f8e1cb944f
Zhengqun Koo zhengqun.koo@gmail.com <Zhengqun Koo zhengqun.koo@gmail.com>
2019-01-01 10:50:35 +0800

    Divide processes into two types

--- ecce/Reactive.hs
+++ ecce/Reactive.hs
@@ -24,6 +24,7 @@ import Base (extractParse)
 import Control.Monad (join, when)
 import Control.Monad.IO.Class (liftIO)
 import Data.Either (isLeft, rights)
+import Debug.Trace (trace)
 import Interpreter (Output, mainHaskeline)
 import Reactive.Banana (accumE, compile)
 import Reactive.Banana.Frameworks
@@ -93,11 +94,19 @@ fire = snd
 {-----------------------------------------------------------------------------
     Program logic
 ------------------------------------------------------------------------------}
--- Each process sequentially resolves transmissions
--- Each process can also have many concurrently running processes
-data Process
-  = ProcessLeaf (Expr GlobalProtocol)
-  | ProcessNode [Process]
+-- A process is either a single GlobalProtocol, or
+-- a list of other processes.
+-- A sequential process resolves processes in order:
+-- resolve the head process, then process the tails.
+-- A concurrent process resolves processes without a defined order.
+data PS
+  = PSLeaf (Expr GlobalProtocol)
+  | PSNode [PC]
+  deriving (Show)
+
+data PC
+  = PCLeaf (Expr GlobalProtocol)
+  | PCNode [PS]
   deriving (Show)
 
 -- Set up the program logic in terms of events and behaviors.
@@ -111,93 +120,40 @@ networkDescription esstepper restInputLine = do
          let gs = map (extractParse parseGlobalProtocol) xs
           in if any isLeft gs
                then putStrLn "some error"
-               else mapM_ putStrLn $
-                    process (ProcessNode $ map ProcessLeaf (rights gs)))
+               else mapM_ putStrLn $ process (PSNode $ map PCLeaf (rights gs)))
       xs
   estepper <- fromAddHandler (addHandler esstepper)
   estep <- accumE 0 $ (+ 1) <$ estepper
   reactimate $ fmap print estep
 
-process :: Process -> [String]
+process :: PS -> [String]
 process p =
-  let aux :: [String] -> Process -> [String]
-      aux ss (ProcessLeaf g)
-        -- trace ("leaf: " ++ show g) $
-       =
+  let auxPS :: [String] -> PS -> [String]
+      auxPS ss (PSLeaf g) =
+        trace ("PSLeaf: " ++ show g) $
         case g of
           EGlobalProtocolConcurrency g1 g2 ->
-            aux
-              ss
-              (ProcessNode
-                 [ProcessNode [ProcessLeaf g1], ProcessNode [ProcessLeaf g2]])
-          EGlobalProtocolChoice g1 g2 -> aux ss (ProcessLeaf g2) -- TODO Unhardcode choice to g2
+            auxPC ss (PCNode [PSLeaf g1, PSLeaf g2])
+          EGlobalProtocolChoice g1 g2 -> auxPS ss (PSLeaf g2) -- TODO Unhardcode choice to g2
           EGlobalProtocolSequencing g1 g2 ->
-            aux ss (ProcessNode [ProcessLeaf g1, ProcessLeaf g2])
+            auxPS ss (PSNode [PCLeaf g1, PCLeaf g2])
           otherwise -> show g : ss
-      aux ss (ProcessNode []) = ss
-      aux ss (ProcessNode (p:ps))
-        -- trace ("node: " ++ show (p : ps)) $
-       = aux (join $ ss : aux [] p : []) (ProcessNode ps)
-   in aux [] p
-{-
-    -- The state of the slot machine is captured in Behaviors.
-        
-    -- State: credits that the player has to play the game
-    -- The  ecoin      event adds a coin to the credits
-    -- The  edoesplay  event removes money
-    -- The  ewin       event adds credits because the player has won
-    (ecredits :: Event Money, bcredits :: Behavior Money)
-        <- mapAccum 0 . fmap (\f x -> (f x,f x)) $ unions $
-            [ addCredit    <$ ecoin
-            , removeCredit <$ edoesplay
-            , addWin       <$> ewin
-            ]
-    let
-        -- functions that change the accumulated state
-        addCredit     = (+1)
-        removeCredit  = subtract 1
-        addWin Double = (+5)
-        addWin Triple = (+20)
-
-        -- Event: does the player have enough money to play the game?
-        emayplay :: Event Bool
-        emayplay = (\credits _ -> credits > 0) <$> bcredits <@> eplay
-
-        -- Event: player has enough coins and plays
-        edoesplay :: Event ()
-        edoesplay = () <$ filterE id  emayplay
-        -- Event: event that fires when the player doesn't have enough money
-        edenied   :: Event ()
-        edenied   = () <$ filterE not emayplay
-
-    -- State: random number generator
-    (eroll :: Event Reels, bstdgen :: Behavior StdGen)
-        -- accumulate the random number generator while rolling the reels
-        <- mapAccum initialStdGen $ roll <$> edoesplay
-
-    let
-        -- roll the reels
-        roll :: () -> StdGen -> (Reels, StdGen)
-        roll () gen0 = ((z1,z2,z3),gen3)
-            where
-            random    = randomR(1,4)
-            (z1,gen1) = random gen0
-            (z2,gen2) = random gen1
-            (z3,gen3) = random gen2
-        
-        -- Event: it's a win!
-        ewin :: Event Win
-        ewin = fmap fromJust $ filterE isJust $ fmap checkWin eroll
-        checkWin (z1,z2,z3)
-            | length (nub [z1,z2,z3]) == 1 = Just Triple
-            | length (nub [z1,z2,z3]) == 2 = Just Double
-            | otherwise                    = Nothing
-
-
-    -- ecredits <- changes bcredits
-    reactimate $ putStrLn . showCredit <$> ecredits
-    reactimate $ putStrLn . showRoll   <$> eroll
-    reactimate $ putStrLn . showWin    <$> ewin
-    reactimate $ putStrLn "Not enough credits!" <$ edenied
-
--}
+      auxPS ss (PSNode []) = ss
+      auxPS ss (PSNode (p:ps)) =
+        trace ("PSNode: " ++ show (p : ps)) $
+        auxPS (join $ ss : auxPC [] p : []) (PSNode ps)
+      auxPC :: [String] -> PC -> [String]
+      auxPC ss (PCLeaf g) =
+        trace ("PCLeaf: " ++ show g) $
+        case g of
+          EGlobalProtocolConcurrency g1 g2 ->
+            auxPC ss (PCNode [PSLeaf g1, PSLeaf g2])
+          EGlobalProtocolChoice g1 g2 -> auxPC ss (PCLeaf g2) -- TODO Unhardcode choice to g2
+          EGlobalProtocolSequencing g1 g2 ->
+            auxPS ss (PSNode [PCLeaf g1, PCLeaf g2])
+          otherwise -> show g : ss
+      auxPC ss (PCNode []) = ss
+      auxPC ss (PCNode (p:ps)) =
+        trace ("PCNode: " ++ show (p : ps)) $
+        auxPC (join $ ss : auxPS [] p : []) (PCNode ps)
+   in auxPS [] p

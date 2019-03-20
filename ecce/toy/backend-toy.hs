ebc9e2483ac5339bcfb574acc000e5c065bdd63b
Zhengqun Koo zhengqun.koo@gmail.com <Zhengqun Koo zhengqun.koo@gmail.com>
2019-03-21 07:10:48 +0800

    Replace GADT with ADT in toy
    --HG--
    branch : invertible-parser

--- ecce/toy/backend-toy.hs
+++ ecce/toy/backend-toy.hs
@@ -1,239 +1,126 @@
 {- SECTION PRAGMAS -}
--- Allows putting `deriving` on a standalone line, needed for GADTs to derive 
--- (Show)
-{-# LANGUAGE StandaloneDeriving #-}
--- Allows constrained ASTs
-{-# LANGUAGE GADTs #-}
--- Allows more than one StandaloneDeriving
-{-# LANGUAGE UndecidableInstances #-}
--- Allows ambiguity check in instance declarations, to use sites
-{-# LANGUAGE AllowAmbiguousTypes #-}
--- Allows datatypes without constructors
-{-# LANGUAGE EmptyDataDecls #-}
+-- Pragmas of invertible-parser
+{-# LANGUAGE TemplateHaskell, NoMonomorphismRestriction #-}
+
+module Toy where
 
 {-
  - SECTION IMPORTS
  -}
-import Control.Monad
-import Data.List (intercalate)
-import System.IO (IOMode(ReadMode), hClose, hGetContents, openFile)
-import Text.Parsec
-import Text.ParserCombinators.Parsec.Expr
-import Text.ParserCombinators.Parsec.Language
-import qualified Text.ParserCombinators.Parsec.Token as Token
+import Control.Category ((.))
+import Control.Isomorphism.Partial ((<$>), cons, inverse, right, subset)
+import Control.Isomorphism.Partial.TH (defineIsomorphisms)
+import Control.Isomorphism.Partial.Unsafe (Iso(..))
+import Data.Char (isDigit, isLetter)
+import Data.List (head)
+import Prelude
+  ( Char
+  , Eq(..)
+  , Integer
+  , Maybe(..)
+  , Show(..)
+  , String
+  , notElem
+  , reads
+  )
+import Text.Syntax
+  ( Syntax
+  , (*>)
+  , (<*)
+  , (<*>)
+  , (<+>)
+  , (<|>)
+  , between
+  , chainl1
+  , many
+  , optSpace
+  , skipSpace
+  , text
+  , token
+  )
+import Text.Syntax.Parser.Naive (parse)
+import Text.Syntax.Printer.Naive (print)
 
 {-
  - SECTION USER INTERFACE
  -}
-main = do
-  inputLines <- interpreter
-  let s = intercalate "\n" $ map (show . extractParse parseExpr) inputLines
-   in putStrLn s
+data Expr
+  = EBool
+  | EInteger Integer
+  | EOpUnary OpUnary
+             Expr
+  | EOpBinary Expr
+              OpBinary
+              Expr
+  deriving (Show)
 
-interpreter :: IO [String]
-interpreter = do
-  mapM_ putStrLn $
-    "Either:" :
-    "1) Type out expression" : "2) Load file (load <file>)" : "" : []
-  inputLine <- getLine
-  inputLines <-
-    if take loadStringLength inputLine == loadString
-      then do
-        handle <- openFile (drop loadStringLength inputLine) ReadMode
-        contents <- hGetContents handle
-        seq (hClose handle) (return $ lines contents)
-      else do
-        return [inputLine]
-  return inputLines
-  where
-    loadString = "load "
-    loadStringLength = length loadString
+data OpUnary
+  = EPureBool
+  | EPureNot
+  | EIntegerNeg
+  deriving (Show, Eq)
 
-{-
- - SECTION TYPES
- -}
-type SParsec = Parsec String ()
+data OpBinary
+  = EPureAnd
+  | EPureOr
+  | EIntegerMul
+  | EIntegerAdd
+  deriving (Show, Eq)
 
-data Pure
+$(defineIsomorphisms ''Expr)
 
-{-
-TyConI
-  (DataD
-     []
-     Main.Expr
-     [KindedTV a_6989586621679078654 StarT]
-     Nothing
-     [ GadtC
-         [Main.EBool]
-         [(Bang NoSourceUnpackedness NoSourceStrictness, ConT GHC.Types.Bool)]
-         (AppT (ConT Main.Expr) (ConT GHC.Types.Bool))
-     , GadtC
-         [Main.EPureBool]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Types.Bool))
-         ]
-         (AppT (ConT Main.Expr) (ConT Main.Pure))
-     , GadtC
-         [Main.EPureAnd]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT Main.Pure))
-         , ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT Main.Pure))
-         ]
-         (AppT (ConT Main.Expr) (ConT Main.Pure))
-     , GadtC
-         [Main.EPureOr]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT Main.Pure))
-         , ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT Main.Pure))
-         ]
-         (AppT (ConT Main.Expr) (ConT Main.Pure))
-     , GadtC
-         [Main.EPureNot]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT Main.Pure))
-         ]
-         (AppT (ConT Main.Expr) (ConT Main.Pure))
-     , GadtC
-         [Main.EInteger]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , ConT GHC.Integer.Type.Integer)
-         ]
-         (AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-     , GadtC
-         [Main.EIntegerNeg]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-         ]
-         (AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-     , GadtC
-         [Main.EIntegerMul]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-         , ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-         ]
-         (AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-     , GadtC
-         [Main.EIntegerAdd]
-         [ ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-         , ( Bang NoSourceUnpackedness NoSourceStrictness
-           , AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-         ]
-         (AppT (ConT Main.Expr) (ConT GHC.Integer.Type.Integer))
-     ]
-     [])
--}
-data Expr a where
-  EBool :: Bool -> Expr Bool
-  EPureBool :: Expr Bool -> Expr Pure
-  EPureAnd :: Expr Pure -> Expr Pure -> Expr Pure
-  EPureOr :: Expr Pure -> Expr Pure -> Expr Pure
-  EPureNot :: Expr Pure -> Expr Pure
-  EInteger :: Integer -> Expr Integer
-  EIntegerNeg :: Expr Integer -> Expr Integer
-  EIntegerMul :: Expr Integer -> Expr Integer -> Expr Integer
-  EIntegerAdd :: Expr Integer -> Expr Integer -> Expr Integer
+$(defineIsomorphisms ''OpUnary)
 
--- Existentially quantify Expr
--- Contains a well-formed Expr, but precise type of Expr is secret
-data AnyExpr where
-  AnyExpr :: Expr a -> AnyExpr
-
-deriving instance Show (Expr a)
-
-instance Show (AnyExpr) where
-  show (AnyExpr a) = show a
-
-anyExpr :: SParsec (Expr a) -> SParsec AnyExpr
-anyExpr e = fmap AnyExpr e
+$(defineIsomorphisms ''OpBinary)
 
 {-
  - SECTION LEXER
  -}
-languageDef =
-  emptyDef
-    { Token.commentStart = "/*"
-    , Token.commentEnd = "*/"
-    , Token.commentLine = "//"
-    , Token.identStart = letter
-    , Token.identLetter = alphaNum
-    , Token.reservedNames = ["true", "false", "~", "^", "v"]
-    , Token.reservedOpNames = ["+", "-", "x", "^", "v", "~"]
-    }
+{- TODO remove this -}
+keywords = ["true", "false", "~", "^", "v"]
 
-lexer = Token.makeTokenParser languageDef
+{- TODO remove this -}
+keywordOps = ["+", "-", "x", "^", "v", "~"]
 
-identifier = Token.identifier lexer -- parses an identifier
+letter, digit :: Syntax delta => delta Char
+letter = subset isLetter <$> token
 
-reserved = Token.reserved lexer -- parses a reserved name
+digit = subset isDigit <$> token
 
-reservedOp = Token.reservedOp lexer -- parses an operator
+identifier =
+  subset (`notElem` keywords) . cons <$> letter <*> many (letter <|> digit)
 
-parens = Token.parens lexer -- parses surrounding parenthesis:
-                                    --   parens p
-                                    -- takes care of the parenthesis and
-                                    -- uses p to parse what's inside them
+keyword :: Syntax delta => String -> delta ()
+keyword s = inverse right <$> (identifier <+> text s)
 
-integer = Token.integer lexer -- parses an integer
+integer :: Syntax delta => delta Integer
+integer = Iso read' show' <$> many digit
+  where
+    read' s =
+      case [x | (x, "") <- reads s] of
+        [] -> Nothing
+        (x:_) -> Just x
+    show' x = Just (show x)
 
-semi = Token.semi lexer -- parses a semicolon
+parens = between (text "(") (text ")")
 
-whiteSpace = Token.whiteSpace lexer -- parses whitespace
+ops = eIntegerMul <$> text "*" <|> eIntegerAdd <$> text "+"
 
-{-
- - SECTION PARSERS
- -}
-extractParse :: SParsec a -> String -> a
-extractParse p s =
-  case parse p "" s of
-    Left x -> error $ show x
-    Right x -> x
+spacedOps = between optSpace optSpace ops
 
-{-
- - SUBSECTION PURE
- -}
-parsePure = buildExpressionParser opPure termPure
+priority :: OpBinary -> Integer
+priority EIntegerMul = 1
+priority EIntegerAdd = 2
 
-opPure =
-  [ [Prefix (reservedOp "~" >> return EPureNot)]
-  , [ Infix (reservedOp "^" >> return EPureAnd) AssocLeft
-    , Infix (reservedOp "|" >> return EPureOr) AssocLeft
-    ]
-  ]
+expression = exp 2
+  where
+    exp 0 =
+      eInteger <$> integer <|> eBool <$> keyword "true" <|>
+      eBool <$> keyword "false" <|>
+      parens (skipSpace *> expression <* skipSpace)
+    exp 1 = chainl1 (exp 0) spacedOps (opPrioBinary 1)
+    exp 2 = chainl1 (exp 1) spacedOps (opPrioBinary 2)
+    opPrioBinary n = eOpBinary . subset (\(x, (op, y)) -> priority op == n)
 
-termPure = parens parsePure <|> try parsePureBool
-
-parsePureBool = do
-  b <- parseBool
-  return $ EPureBool b
-
-{-
- - SUBSECTION BOOL
- -}
-parseBool =
-  (reserved "true" >> return (EBool True)) <|>
-  (reserved "false" >> return (EBool False))
-
-{-
- - SUBSECTION INT
- -}
-parseInteger = buildExpressionParser opInteger termInteger
-
-opInteger =
-  [ [Prefix (reservedOp "-" >> return EIntegerNeg)]
-  , [Infix (reservedOp "x" >> return EIntegerMul) AssocLeft]
-  , [Infix (reservedOp "+" >> return EIntegerAdd) AssocLeft]
-  ]
-
-termInteger = parens parseInteger <|> liftM EInteger integer
-
-{-
- - SUBSECTION EXPR
- -}
-parseExpr :: SParsec AnyExpr
-parseExpr = do
-  e <- try (anyExpr parsePure) <|> try (anyExpr parseInteger)
-  return e
+main = print expression (head (parse expression "1+2"))
+-- main = print expression (EOpBinary (EInteger 1) EIntegerAdd (EInteger 2))

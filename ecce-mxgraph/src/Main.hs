e69d50e4b65d10e82a3373d63fd87a1378b2b3ab
zhengqunkoo <root@zhengqunkoo.com>
2019-11-24 12:23:41 +0100

    Get Main.hs to compile

--- ecce-mxgraph/src/Main.hs
+++ ecce-mxgraph/src/Main.hs
@@ -1,10 +1,46 @@
+{-
+ - SECTION PRAGMAS
+ -}
+{-# LANGUAGE GADTs #-} -- Allows patterm match on GADT
+
+{-
+ - SECTION MODULE
+ -}
 module Main where
 
+{-
+ - SECTION IMPORTS
+ -}
+import Control.Monad (join)
 import Data.JSString (JSString, pack)
-import Ecce.Parser (extractFile)
-import Ecce.Processor (networkDescription)
+import Data.List (intercalate, nub)
+import Ecce.Parser
+  ( AnyExpr(AnyExpr)
+  , AnyExpr
+  , Expr(..)
+  , GlobalProtocol
+  , parseExpr
+  )
+import Ecce.Processor
+  ( Process
+  , eventLoop
+  , mayProcessToGlobalProtocol
+  , networkProcessor
+  , parseContents
+  , projectGlobalToEndpoint
+  )
+import Ecce.Projector (projectPartyToEndpoint)
 import Ecce.Unparser (un)
-import Reactive.Banana (Behavior, Event, Moment, liftMoment)
+import Reactive.Banana (Behavior, Event, Moment, accumE, compile, liftMoment)
+import Reactive.Banana.Frameworks
+  ( MomentIO
+  , actuate
+  , changes
+  , fromAddHandler
+  , newAddHandler
+  , reactimate
+  , reactimate'
+  )
 
 foreign import javascript unsafe "new Show().show($1)" mx_show
   :: JSString -> IO ()
@@ -49,9 +85,9 @@ networkPrinter ::
      , Behavior Int)
   -> MomentIO ()
 networkPrinter (eTrans, bProc, eDone, bStepCount) = do
-  eTransAcc <- accumE [] ((:) <$> eTrans)
-  reactimate $
-    maybe (return ()) (mx_show . pack . putStrLn . un . AnyExpr) <$> eTransAcc
+  eTransAcc <-
+    accumE EGlobalProtocolEmp (maybe id EGlobalProtocolSequencing <$> eTrans)
+  reactimate $ (mx_show . pack . un . AnyExpr) <$> eTransAcc
   eProc <- changes bProc
   -- TODO find more efficient way of getting endpoint protocols
   reactimate' $
@@ -62,8 +98,25 @@ networkPrinter (eTrans, bProc, eDone, bStepCount) = do
        map (un . AnyExpr) . projectGlobalToEndpoint . mayProcessToGlobalProtocol) <$>
     eProc
   eStepCount <- changes bStepCount
-  reactimate' $ fmap (putStrLn . show) <$> eStepCount
-  reactimate $ putStrLn "Done!" <$ eDone
+  reactimate' $ fmap (mx_show . pack . show) <$> eStepCount
+  reactimate $ (mx_show . pack) "Done!" <$ eDone
+
+networkDescription :: Event Char -> MomentIO ()
+networkDescription eKey =
+  (liftMoment $
+   networkProcessor
+     (fmap
+        head
+        (parseContents $
+         Right
+           [ "b1--(1)->s:s<1.E1.emp^1:order>; (s--(2)->b1:b1<2.E2.emp^2:price>*s--(3)->b2:b2<3.E3.emp^3:price>); b1--(4)->b2:b2<4.E4.emp^4:amt>; (b2--(5)->s:s<5.E5.emp^5:no>| (b2--(6)->s:s<6.E6.emp^6:yes>; b2--(7)->s:s<7.E7.emp^7:addr>))"
+           ]))
+     (Just <$> eKey)) >>=
+  networkPrinter
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (addKeyEvent, fireKey) <- newAddHandler
+  network <- compile $ fromAddHandler addKeyEvent >>= networkDescription
+  actuate network
+  eventLoop fireKey network
